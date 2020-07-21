96334febeb09eb409bfa505e1b26420d61e67091
Tommy Waalkes <Tommy@grandcircus.co>
2020-07-21 09:42:19 -0400

    Initial Commit

---
+++ UploadTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UploadTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This looks ready to ship");
+        }
+    }
+}
